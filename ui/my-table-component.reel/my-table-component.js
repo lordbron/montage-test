e8dc36c39e9d1f73f676c04a7818f640950012db
lordbron <noreply>
2018-06-11 22:05:24 +0000

    Add component my-table-component

---
+++ ui/my-table-component.reel/my-table-component.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/my-table-component.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyTableComponent
+ * @extends Component
+ */
+exports.MyTableComponent = Component.specialize(/** @lends MyTableComponent.prototype */{
+
+});
